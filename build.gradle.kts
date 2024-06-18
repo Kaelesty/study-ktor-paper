395d77013c9dffc49a5206853bde405cd4fbfd43
Ilya Bundelev <kayrismerc@yandex.ru>
2024-06-18 16:37:33 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.11"
+}
+
+group = "com.paper"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.paper.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-cio-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+}
